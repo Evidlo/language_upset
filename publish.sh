b8f9bf7e980755159d8973928d74218341473c0c
Evidlo <evan+gh@evanw.org>
2026-01-13 14:55:41 -0600

    add publish script

---
+++ publish.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+npm run build
+npm run buildsets
+
+mkdir dist/
+cp bundle.js index.html sets.json combinations.json dist/
+touch dist/.nojekyll
+
+# copy contents of dist/ to root of publish branch
+ghp-import -f -p -b publish dist/
